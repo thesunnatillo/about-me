888f12d732f671b8542e6b56a690b9a6987018b3
thesunnatillo <sunnatillocoder@gmail.com>
2025-09-07 14:28:48 +0500

    feat: add swagger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,55 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+const globals = require("globals");
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+module.exports = [
+    {
+        ignores: ["**/*.config.js", "node_modules/**", "dist/**", "**/*spec.ts"],
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: require("@typescript-eslint/parser"),
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+            parserOptions: {
+                project: ["apps/**/tsconfig.app.json", "globals/tsconfig.lib.json", "libs/**/tsconfig.lib.json"],
+                tsconfigRootDir: __dirname,
+            },
+        },
+        plugins: {
+            "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+            "@stylistic": require("@stylistic/eslint-plugin"),
+        },
+        rules: {
+            "@typescript-eslint/interface-name-prefix": "off",
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-undef": "off",
+            "@typescript-eslint/no-floating-promises": ["error"],
+            "@typescript-eslint/no-unused-vars": ["error", { vars: "all", args: "none" }],
+            "no-var-requires": "off",
+            "@typescript-eslint/no-empty-function": 0,
+            "@stylistic/comma-dangle": ["error"],
+            "@stylistic/comma-spacing": ["error"],
+            "@stylistic/member-delimiter-style": ["error"],
+            "@stylistic/no-extra-semi": ["error"],
+            "@typescript-eslint/no-for-in-array": ["error"],
+            "@typescript-eslint/no-use-before-define": ["error"],
+            "@typescript-eslint/no-useless-constructor": ["error"],
+            "@stylistic/space-before-blocks": ["error"],
+            "@typescript-eslint/require-await": ["error"],
+            "padded-blocks": ["error", { blocks: "always", classes: "always", switches: "always" }],
+            "no-multiple-empty-lines": ["error", { max: 1, maxBOF: 1 }],
+            "no-useless-catch": "error",
+            semi: ["error"],
+            "object-curly-spacing": ["error", "always"],
+            "no-empty": ["error"],
+            indent: ["error", 4, { SwitchCase: 1 }],
+            "@stylistic/no-multi-spaces": ["error", { ignoreEOLComments: false }],
+            "@stylistic/type-annotation-spacing": ["error", { before: false, after: true }],
+            "@stylistic/semi-spacing": ["error"],
+        },
     },
-  },
-);
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sunnatillo Sharipov')
+    .setDescription('19 y.o Node.js backend developer')
+    .setVersion('19v')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
+
 bootstrap();
