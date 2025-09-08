bd3bcccb42f10ce570ea67dada1d04d7880c7f2b
thesunnatillo <sunnatillocoder@gmail.com>
2025-09-08 16:29:24 +0500

    quote

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<h3>now i am become death, the destroyer of worlds.</h3>';
   }
 }
